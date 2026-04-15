eccefd013847c3263f2a0cde6a25a683eefbdee1
Roy Zeng <142411359+RoyZeng0317@users.noreply.github.com>
2026-04-15 20:13:17 +0800

    Add files via upload

--- BB Call Call back/BB_Call/BB_Call.ino
+++ BB Call Call back/BB_Call/BB_Call.ino
@@ -1,23 +1,43 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // sethe LCD address to 0x27 for a 16 chars and line display
 
-char response = {"520", "521", "I miss you"};
+Strin[] response = {"520", "521", "I miss you"};
+
+SoftwareSerial BT(10, 11);
+
+String received = "";
 
 void setup() {
   // put your setup code here, to run once:
   lcd.init(); // initialize the lcd
-  // lcd.begin(16, 2);
   lcd.backlight();
 
+  BT.begin(9600);
+  Serial.begin(9600);
+
   lcd.setCursor(0, 0);
-  lcd.print("Hello, world!");
-  
-  lcd.setCursor(0, 1);
-  lcd.print("BBC Call");
+  lcd.print("BB Call Ready");  
 }
 
 void loop() {
+  while(BT.available()){
+    char c = BT.read();
+    received += c;
 
+    if(c == '\n'){
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Msg:");
+
+      lcd.setCursor(0, 1);
+      lcd.print(received);
+
+      BT.println("Received: " + received);
+
+      received = "";
+    }
+  }
 }
