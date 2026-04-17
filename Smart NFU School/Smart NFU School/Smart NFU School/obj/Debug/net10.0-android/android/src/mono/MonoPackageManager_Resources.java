d531bb02222171d295db38b3919225b0b3bfcdb5
Roy Zeng <142411359+RoyZeng0317@users.noreply.github.com>
2026-04-17 15:55:28 +0800

    Upload
    Upload the mobile phone interface

---
+++ Smart NFU School/Smart NFU School/Smart NFU School/obj/Debug/net10.0-android/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,19 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "Smart NFU School.dll" comes first in this list. */
+		"Smart NFU School.dll",
+		"Microsoft.Maui.Controls.HotReload.Forms.dll",
+		"Microsoft.VisualStudio.DesignTools.MobileTapContracts.dll",
+		"Microsoft.VisualStudio.DesignTools.TapContract.dll",
+		"Microsoft.VisualStudio.DesignTools.XamlTapContract.dll",
+		"_Microsoft.Android.Resource.Designer.dll",
+		"Microsoft.Maui.Controls.HotReload.Forms.dll",
+		"Microsoft.VisualStudio.DesignTools.MobileTapContracts.dll",
+		"Microsoft.VisualStudio.DesignTools.TapContract.dll",
+		"Microsoft.VisualStudio.DesignTools.XamlTapContract.dll",
+		"_Microsoft.Android.Resource.Designer.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
